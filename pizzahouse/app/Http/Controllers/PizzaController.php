40193240b9f27c6a2970f748d6d6675219a2a6e7
haianhdt98 <phamhaianhdt98@gmail.com>
2020-07-30 11:24:27 +0700

    add #20 of Tutorial

--- pizzahouse/app/Http/Controllers/PizzaController.php
+++ pizzahouse/app/Http/Controllers/PizzaController.php
@@ -3,28 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Pizza;
 
 class PizzaController extends Controller
 {
     public function index() {
         // get data from database
-        $pizzas = [
-            ['type' => 'Hawaiian', 'base' => 'cheesy crust'],
-            ['type' => 'Volcano', 'base' => 'glatic crust'],
-            ['type' => 'Veg pere', 'base' => 'thin & scream']
-        ];
+        // $pizzas = Pizza::all();
+        // $pizzas = Pizza::orderBy('name', 'asc')->get();
+        // $pizzas = Pizza::where('type', 'Hawaiian')->get();
+        $pizzas = Pizza::latest()->get();
 
-        $name = request('name');
-        $age = request('age');
-
-        return view('pizzas', [
+        return view('pizzas.index', [
             'pizzas' => $pizzas,
-            'name' =>   $name,
-            'age' =>    $age,
         ]);
     }
 
     public function show($id) {
-        return view('details', ['id' => $id]);
+
+        $pizza = Pizza::findOrFail($id);
+
+        return view('pizzas.show', ['pizza' => $pizza]);
+    }
+
+    public function create() {
+        return view('pizzas.create');
+    }
+
+    public function store() {
+
+        $pizza = new Pizza();
+
+        $pizza->name = request('name');
+        $pizza->type = request('type');
+        $pizza->base = request('base');
+
+        $pizza->save();
+
+        return redirect('/')->with('mssg', 'Thanks for your order');
     }
 }
--- pizzahouse/resources/views/welcome.blade.php
+++ pizzahouse/resources/views/welcome.blade.php
@@ -21,17 +21,9 @@
         <div class="title m-b-md">
             The Pizza House
         </div>
-
-        <div class="links">
-            <a href="#">Docs</a>
-            <a href="https://laracasts.com">Laracasts</a>
-            <a href="https://laravel-news.com">News</a>
-            <a href="https://blog.laravel.com">Blog</a>
-            <a href="https://nova.laravel.com">Nova</a>
-            <a href="https://forge.laravel.com">Forge</a>
-            <a href="https://vapor.laravel.com">Vapor</a>
-            <a href="https://github.com/laravel/laravel">GitHub</a>
-        </div>
+        <p class="mssg">{{ session('mssg') }}</p>
+        <a href="/pizzas/create">Order a Pizza</a>
+        
     </div>
 </div>
 @endsection
